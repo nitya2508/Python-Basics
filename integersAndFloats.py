08d004f2a427e6cf00ad5d2fdc6e7ac48f718dff
nitya2508 <nityashree2508@gmail.com>
2023-07-19 06:02:18 +0530

    arithmetic operators and comparisions

---
+++ integersAndFloats.py
@@ -0,0 +1,37 @@
+num = 3
+num2 =3.12
+
+print(type(num),type(num2))
+
+#Arithmetic operators
+print(3 + 2) #addition
+print(3 - 2) #subtraction
+print(3 * 2) #mulyiplication
+print(3 / 2) #division
+print(3 // 2) #foor division
+print(3 ** 2) #exponent
+print(3 % 2) #modulus -- gives the reminder
+
+print(abs(-5))
+print(round(num2,1))
+
+#comparisions
+num_1 = 3
+num_2 = 2
+
+print(num_1 == num_2)
+print(num_1 != num2)
+print(num_1 < num2)
+print(num_1 > num2)
+print(num_1 <= num2)
+print(num_1 >= num2)
+
+#casting
+number1 = '100'
+number2 = '200'
+
+print(number1 + number2)
+number1 = int(number1)
+number2 = int(number2)
+
+print(number1 + number2)

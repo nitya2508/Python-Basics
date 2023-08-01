279b44059f39da8057210b0f0e21c3d639cb0ea6
nitya2508 <nityashree2508@gmail.com>
2023-08-01 05:50:37 +0530

    for , while loops basics

---
+++ loops.py
@@ -0,0 +1,39 @@
+num= [1,2,3,4,5]
+
+# break and continue statements
+
+for i in num:
+    if i == 3:
+        print("found")
+        break
+    print(i)
+
+for i in num:
+    if i == 3:
+        continue
+    print(i)
+
+# nested for loop
+
+# for i in num:
+#     for char in 'abc':
+#         print (i, char)
+
+# range method
+
+# for i in range(10):
+#     print(i)
+
+# specify the start in range
+
+for i in range(2,10):
+    print(i)
+
+# while loop
+n = 50
+while n<60:
+    if n == 55:
+        break
+    print(f'inside while {n}')
+    n+=1
+
